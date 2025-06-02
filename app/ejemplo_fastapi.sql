91a9185b7428eec36e46cf5d545791e91ede7172
AlonsoFGJ <alo.gonzalezj@duocuc.cl>
2025-06-01 23:09:21 -0400

    Actualizacion Final 01-5
    Actualizacion Final

--- app/ejemplo_fastapi.sql
+++ app/ejemplo_fastapi.sql
@@ -1,10 +1,29 @@
-DROP TABLE cliente;
-CREATE TABLE cliente(
-    rut NUMBER(8) PRIMARY KEY,
-    nombre VARCHAR2(100) NOT NULL,
-    email VARCHAR2(100) NOT NULL
+DROP TABLE usuario;
+
+CREATE TABLE usuario(
+    rut VARCHAR2(10) PRIMARY KEY,
+    tipo_usuario VARCHAR2 (15) NOT NULL,
+    p_nombre VARCHAR2 (20) NOT NULL,
+    s_nombre VARCHAR2(20),
+    p_apellido VARCHAR2 (20) NOT NULL,
+    s_apellido VARCHAR2 (20),
+    direccion VARCHAR2 (100) NOT NULL,
+    correo VARCHAR2(100) NOT NULL,
+    contra VARCHAR2 (30) NOT NULL    
 );
-INSERT INTO cliente VALUES(12222,'Lalo','lalo@gmail.com');
-INSERT INTO cliente VALUES(13333,'Elvis','elvis@gmail.com');
-INSERT INTO cliente VALUES(14444,'Maria','maria@gmail.com');
+
+INSERT INTO usuario VALUES('20876932-3', 'admin', 'Roberto' , null, 
+'Pacheco', 'Ruiz', 'Av. Mexico 2051', 'roberto@gmail.com', 'roberto123');
+
+INSERT INTO usuario VALUES('17234894-8', 'vendedor', 'Ricardo' , 'Milano', 
+'Aguilera', 'Manquian', 'Pedro Leon Gallo 1929', 'ricardo@gmail.com', 'ricardo123');
+
+INSERT INTO usuario VALUES('19487923-5', 'contador', 'Raquel' , null, 
+'Muñoz', 'Rojas', 'Villa Cariño 2969', 'raquel@gmail.com', 'raquel123');
+
+INSERT INTO usuario VALUES('20347632-7', 'bodega', 'Enrique' , 'Pavel', 
+'Iglesias', 'Morales', 'Av. Brasil 7820', 'enrique@gmail.com', 'enrique123');
+
+INSERT INTO usuario VALUES('10033190-k', 'invitado', 'Ximena' , 'Alberta', 
+'Ilabaca', 'Silva', 'Blest Gana 2270', 'ximena@gmail.com', 'ximena123');
 COMMIT;
